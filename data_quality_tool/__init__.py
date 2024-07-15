4238a2f1ccc812f757e79fd1c7587d66315c2c36
xifanzou <xifanzou42@gmail.com>
2024-07-15 14:48:24 +0800

    update v.0.2

---
+++ data_quality_tool/__init__.py
@@ -0,0 +1,3 @@
+from .scripts import check_and_export, generate_standard_file
+
+__all__ = ['check_and_export', 'generate_standard_file']
--- data_quality_tool/__main__.py
+++ data_quality_tool/__main__.py
@@ -1,52 +1,34 @@
 import os
-import sys
-from data_quality_tool.scripts import get_current_date_indicator, check_and_export
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: data_check <data_folder_path>")
-        sys.exit(1)
-    
-    data_folder_path = sys.argv[1]
-    date_indicator = get_current_date_indicator()
-    log_file_path = os.path.join(data_folder_path, f'check_log_{date_indicator}.csv')
-    
-    check_and_export(data_folder_path, log_file_path)
-
-if __name__ == '__main__':
-    main()
-
-
 import argparse
 import sys
-from data_quality_tool import check_and_export, generate_standard_file, get_current_date_indicator
+from data_quality_tool.scripts import check_and_export, generate_standard_file, get_current_date_indicator
 
 def main():
     parser = argparse.ArgumentParser(description='Data Quality Tool')
     subparsers = parser.add_subparsers(dest='command')
 
     # Sub-parser for the check command
-    check_parser = subparsers.add_parser('--check', help='Check data quality and export log')
+    check_parser = subparsers.add_parser('check', help='Check data quality and export log')
     check_parser.add_argument('data_folder', type=str, help='Path to the folder containing data files and standard file')
 
     # Sub-parser for the generate command
-    generate_parser = subparsers.add_parser('--generate', help='Generate data value standard file')
+    generate_parser = subparsers.add_parser('generate', help='Generate data value standard file')
     generate_parser.add_argument('data_folder', type=str, help='Path to the folder to save the standard file')
     generate_parser.add_argument('--input_file', type=str, help='Path to the input CSV data file (optional)', default=None)
 
     args = parser.parse_args()
 
-    if args.command == '--check':
+    if args.command == 'check':
         date_indicator = get_current_date_indicator()
         log_file_path = os.path.join(args.data_folder, f'check_log_{date_indicator}.csv')
         check_and_export(args.data_folder, log_file_path)
-    elif args.command == '--generate':
+    elif args.command == 'generate':
         generate_standard_file(args.data_folder, args.input_file)
     else:
         print("Usage: data_check <command> [options]")
         print("Commands:")
-        print("  --check <data_folder>  Check data quality and export log")
-        print("  --generate <data_folder> [--input_file <input_file>]    Generate data value standard file")
+        print("  check <data_folder>  Check data quality and export log")
+        print("  generate <data_folder> [--input_file <input_file>]    Generate data value standard file")
         sys.exit(1)
 
 if __name__ == '__main__':
